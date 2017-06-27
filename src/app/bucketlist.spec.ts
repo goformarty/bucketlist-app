df2d0e7f6d77962beaf68e1c5d167aad9f395b0a
Marty <marty.stepien@gmail.com>
2017-06-27 15:06:53 +0100

    first test added

--- src/app/bucketlist.spec.ts
+++ src/app/bucketlist.spec.ts
@@ -4,4 +4,12 @@ describe('Bucketlist', () => {
   it('should create an instance', () => {
     expect(new Bucketlist()).toBeTruthy();
   });
+  it('should accept values in the constructor', () => {
+    let bucketlist = new Bucketlist({
+      title: 'hello';
+      complete: true;
+    });
+    expect(bucketlist.title).toEqual('hello');
+    expect(bucketlist.complete).toEqual(true);
+  });
 });
--- src/app/bucketlist.ts
+++ src/app/bucketlist.ts
@@ -2,4 +2,8 @@ export class Bucketlist {
   id: number;
   title: string;
   complete: boolean;
+
+  constructor(values: Object = {}) {
+    Object.assign(this, values);
+  }
 }
