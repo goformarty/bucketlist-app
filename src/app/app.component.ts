85f773d7acb77ecca709a715da4c2c1e907ba98a
Marty <marty.stepien@gmail.com>
2017-06-28 12:17:30 +0100

    editing app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Bucketlist} from './bucketlist';
+import {BucketlistDataService} from './bucketlist-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [BucketlistDataService]
 })
 export class AppComponent {
-  title = 'app works!';
+
+  newBucketlist: Bucketlist = new Bucketlist();
+
+  constructor(private todoDataService: BucketlistDataService) {
+  }
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newBucketlist);
+    this.newBucketlist = new Bucketlist();
+  }
+
+  toggleTodoComplete(todo) {
+    this.todoDataService.toggleTodoComplete(todo);
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
